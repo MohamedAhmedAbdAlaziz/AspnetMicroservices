b5ad178f98b11fdc1a1ee8be015568b5c1416cc1
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2024-10-10 16:59:59 +0300

    add exceptions

---
+++ Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation.Results;
+using FluentValidation.TestHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Exceptions
+{
+    public class ValidationException :ApplicationException
+    {
+        public ValidationException()
+        :base("One Or more Validation failures have occrred.")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+          public ValidationException(IEnumerable<ValidationFailure> validationFailures)
+        :this()
+        {
+            Errors = validationFailures.GroupBy(e=>e.PropertyName , e=>e.ErrorMessage)
+                .ToDictionary(f=> f.Key, f=>f.ToArray());
+        }
+
+        public IDictionary<string, string[]> Errors { get; }
+
+    }
+}
