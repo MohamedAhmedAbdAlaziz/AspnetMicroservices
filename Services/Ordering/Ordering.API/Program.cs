efcd9ebbc16ec17900f635c7c1a66d0c4929a7c9
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2024-10-15 23:48:05 +0300

    some seeds

--- Services/Ordering/Ordering.API/Program.cs
+++ Services/Ordering/Ordering.API/Program.cs
@@ -1,4 +1,7 @@
+using Ordering.API.Extensions;
 using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-  builder.Services.AddApplicationServices();
+  builder.Services.AddApplicationServices().AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -22,4 +25,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app.MigrateDatabase<OrderContext>((context, services) =>
+{
+    var logger = services.GetService<ILogger<OrderContextSeed>>();
+    OrderContextSeed
+        .SeedAsync(context, logger)
+        .Wait();
+}).Run();
--- Services/Ordering/Ordering.Application/Models/EmailSettings.cs
+++ Services/Ordering/Ordering.Application/Models/EmailSettings.cs
@@ -8,5 +8,8 @@ namespace Ordering.Application.Models
 {
     public class EmailSettings
     {
+        public string ApiKey { get; set; }
+        public string FromAddress { get; set; }
+        public string FromName { get; set; }
     }
 }
