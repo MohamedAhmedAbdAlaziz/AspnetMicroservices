142a76c1387585eee0b4ef005b2cf464766332b1
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2024-10-19 15:19:29 +0300

    Add EndPoint For ChechingOut Basket

--- aspnetrun-microservicies/Services/Basket/Basket.API/Program.cs
+++ aspnetrun-microservicies/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,8 @@
 using Basket.API.GrpcServices;
+using Basket.API.Mapper;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
+builder.Services.AddAutoMapper(typeof(BasketProfile));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,6 +28,13 @@ builder.Services.AddGrpc(options =>
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
     (o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
 builder.Services.AddScoped<DiscountGrpcService>();
+builder.Services.AddMassTransit(config => {
+    config.UsingRabbitMq((ctx, cfg) => {
+        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+    });
+});
+builder.Services.AddMassTransitHostedService();
+
 
 var app = builder.Build();
 
