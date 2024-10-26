0b7a89476d5de16d687dc3476bd45950536f5b78
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2024-10-27 02:24:01 +0300

    add ocelot services

--- ApiGateways/OcelotApiGw/Program.cs
+++ ApiGateways/OcelotApiGw/Program.cs
@@ -3,19 +3,24 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add Ocelot services to the DI container
 builder.Services.AddOcelot();
 
-var app = builder.Build();
-
+// Load Ocelot configuration based on the environment
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
 
 // Configure logging
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
-builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
+
+var app = builder.Build();
+
+// Use Ocelot middleware to handle routing
 await app.UseOcelot();
+
+// Map a simple health check route
 app.MapGet("/", () => "Hello World!");
 
-
+// Run the application
 app.Run();
